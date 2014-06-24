6c8eef2d40616f18507b83dc58f956090f31a07f
John Doe <mmcclure@brightcove.com>
2014-06-24 00:05:30 -0700

    removed fontcustom things

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    webfont: {
+      icons: {
+        src: 'vectors/*.svg',
+        dest: 'build/fonts',
+        destCss: 'build/css',
+        syntax: 'bem',
+        options: {
+           font: 'videojs'
+        },
+        templateOptions: {
+            baseClass: 'vjs-icon',
+            classPrefix: 'vjs_',
+            mixinPrefix: 'vjs-'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-webfont');
+
+  // Default task(s).
+  grunt.registerTask('default', ['webfont']);
+
+};
